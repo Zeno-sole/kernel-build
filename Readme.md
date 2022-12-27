19d073cdc2d286f13d8bc3d63d00517c905d2763
李成刚 <lichenggang@uniontech.com>
2022-12-27 17:54:26 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# deepin-riscv-board
+
+[![Build 
+Status](https://api.cirrus-ci.com/github/Zeno-sole/kernel-build.svg)](https://cirrus-ci.com/github/Zeno-sole/kernel-build)
+
+
+## Ref
+[sehraf/riscv-arch-image-builder](https://github.com/sehraf/riscv-arch-image-builder)
---
+++ scripts/machine_init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+machine_info() {
+    uname -a
+    echo $(nproc)
+    lscpu
+    whoami
+    env
+    fdisk -l
+    df -h
+}
+
+init() {
+    # Init out folder & rootfs
+    mkdir -p ${OUT_DIR}
+    mkdir -p rootfs
+
+    apt update
+
+    # create flash image
+    fallocate -l 7G rootfs.img
+}
+
+machine_info
+init
